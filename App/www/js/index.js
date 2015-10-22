c38bdb18bf7cb2686954b15e9598d291448b6f45
Laudivan Freire de Almeida <laudivan@bigeyessolution.com>
2015-10-22 14:31:50 -0300

    copiando os arquivos

--- App/www/js/index.js
+++ App/www/js/index.js
@@ -1,10 +1,24 @@
-<<<<<<< HEAD
 var gaPlugin = false;
-document.addEventListener("deviceready", onDeviceReady, false);
+var showVotacao = false;
+var onLine = false;
+var watchID = false;
+var edesiolat = 0;
+var edesiolng = 0;
+var raio = 0;
 
 function onDeviceReady () {
+    //followPosition ();
     
+    gaPlugin = window.plugins.gaPlugin;
+    
+    if (gaPlugin) {
+        gaPlugin.init(gaSuccessHandler, gaErrorHandler, "UA-59363254-3", 10); 
+    }
 }
+
+function setOnLine () { onLine = true; }
+
+function setOffLine () { onLine = false; }
 
 function aviso () {
     
@@ -22,11 +36,34 @@ function showHideBtnJuri () {
     
 }
 
+function isConnected () {
+    
+}
+
+function followPosition () {
+    watchID = navigator.geolocation.watchPosition(
+            checkUserPosition, geolocationError,
+    {
+        enableHighAccuracy: true,
+        timeout: 1000,
+        maximumAge: 5000
+    });
+}
+
+function checkUserPosition (position) {
+    //position.coords.latitude, position.coords.longitude
+    
+    //
+}
+
+function geolocationError (error) {
+    
+}
+
 function gaSuccessHandler() { }
 
 function gaErrorHandler() { }
-=======
-$.getJSON("http://192.168.0.3/info.php",function( json ) {
-  console.log( "JSON Data: " + json );
- });
->>>>>>> 75889780fbccd2fb891277e457d44bb997daa56b
+
+document.addEventListener("deviceready", onDeviceReady, false);
+document.addEventListener("online", setOnLine, false);
+document.addEventListener("offline", setOffLine, false);
