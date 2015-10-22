aa43c4bdabf5d233b39bf457eb770a8aa1f8284f
washington azevedo <washingtonlsazevedo@hotmail.com>
2015-10-22 02:38:17 -0300

    Finish FrontEnd

--- App/www/js/index.js
+++ App/www/js/index.js
@@ -1,7 +1,3 @@
-$(
-    $('.ui-page').scroll(function () {
-        //.scrollTop()
-        //$( ":mobile-pagecontainer" )
-    });
-    
-);
+$.getJSON("http://192.168.0.3/info.php",function( json ) {
+  console.log( "JSON Data: " + json );
+ });
